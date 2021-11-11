27cb4dfc8d96f11a610cd4d38880bda35d00a50f
Brandon <branjuny11@gmail.com>
2021-11-10 19:27:25 -0600

    primer paso del programa

--- Examen_1GIT.py
+++ Examen_1GIT.py
@@ -1,7 +1,33 @@
 """
-Aqui inician los cambios
+A continuacion presentamos el panel de neustra empresa
+
 """
+print("------------------EMPRESA-----------------------")
+print("_______________BJ-CORP_UNITE___________________")
+print("Seleccione el rango al que pertenece")
+print("CEO-(1)\nSU_CEO-(2)\nGERENTE-(3)\nJEFE DE AREA-(4)\nEMPLEADO-(5)")
 
-print("Se plantea la siguiente situacion pon atencion")
+while True:
+    Opcion = input("Opcion: ")
 
-print(" La rama Examen_2")
+    try:
+        Opcion = int(Opcion)
+        break
+    except ValueError:
+        print("Por favor ingrese el numero de su rango correctamente")
+
+if Opcion == 1:
+    print("Bienvenido CEO")
+
+if Opcion == 2:
+    print("Bienvenido SUB_CEO")
+
+if Opcion == 3:
+    print("Bienvenido GERENTE")
+
+if Opcion == 4:
+    print("Bienvenido JEFE DE AREA")
+
+if Opcion == 5:
+    print("Bienvenido EMPLEADO")
+
