82da7a071e3540a7ea00befb6557c6b316d8a9a1
Brandon <branjuny11@gmail.com>
2021-11-11 21:13:56 -0600

    Segimos en CEO

--- Examen_1GIT.py
+++ Examen_1GIT.py
@@ -3,6 +3,8 @@ A continuacion presentamos el panel de neustra empresa
 """
 import main as Examen
 import main2 as Examen2
+import main3 as Examen3
+
 Empresa1 = Examen.Empresa("BJ-CORP_UNITE","UNITE STATE")
 Empresa1.Descripcion()
 #print("------------------EMPRESA-----------------------")
@@ -27,12 +29,17 @@ if Opcion < 1:
 if Opcion == 1:
     print("  ")
     print("Bienvenido CEO")
-    Persona1 = Examen2.Empleado("Brandon J. Juarez Gervacio", 45, 1.60, 60, 100000,20,"Masculino")
+    Persona1 = Examen2.Empleado("Brandon J. Juarez Gervacio", 45, 1.60, 100000,20,"Masculino")
     Persona1.describir()
+
+    print(" ")
     print("La vestimenta propuesta por la empresa para usted es : ")
-    #Se inserta el main y se crea su vestimenta
-    print("Las tareas para el dia de hoy son las siguientes: ")
-    CEO1 = Examen.CEO("CEO de la empresa",1 ,"indefinidas","SI","NO","SI",0.0,2)
+    Vestimenta1 = Examen3.CEO("Negro","Negro","Blanco","Roja","Negro","Zapato","Casual pelo corto")
+    Vestimenta1.Descrip()
+
+    print(" ")
+    print("Las tareas para el dia de hoy son las siguientes")
+    CEO1 = Examen.CEO("CEO de la empresa","001" ,"Indefinidas","Si","No","Si",0,2)
     CEO1.Descripcion()
 
 if Opcion == 2:
